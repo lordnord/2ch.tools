2b4b9e93823bcdb6a41d9ae41d6f72d1c8d0e3b5
lordnord <dlx1989@gmail.com>
2017-01-20 20:43:16 +0300

    pep8 styling

--- dumper.py
+++ dumper.py
@@ -4,28 +4,29 @@ import os
 import sys
 import time
 import random
-
 import requests
-requests = requests.Session()
 
 import funcs
 
 NUMBER = 4
 PAUSE = 20
 
+session = requests.Session()
 _, thread, folder = sys.argv
+del _
 all = funcs.accepted_files(folder)
 
 while len(all) >= NUMBER:
-    files = {}
+    images = {}
     for i in range(NUMBER):
         filename = random.choice(all)
         all.remove(filename)
         path = os.path.join(folder, filename)
-        files['image{}'.format(i + 1)] = open(path, 'rb').read()
-    response = requests.post('https://2ch.hk/makaba/posting.fcgi', 
-                            data=funcs.basic_data(thread),  
-                            files=files
-                            )
+        images['image{}'.format(i + 1)] = open(path, 'rb').read()
+    response = session.post(
+        'https://2ch.hk/makaba/posting.fcgi', 
+        data = funcs.basic_data(thread),  
+        files = images,
+        )
     print (response.status_code)
     time.sleep(PAUSE)
